c7d6a6a5216cfc3db13cae909a675501a205b0fd
Tristan de Boer <info@tristandeboer.nl>
2023-01-10 14:10:41 +0100

    Update totals.blade.php

--- resources/views/totals.blade.php
+++ resources/views/totals.blade.php
@@ -1,11 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<iframe
-    src="http://95.170.68.115:32768/public/dashboard/4c170266-46f8-4f8f-91df-8a56d730ca41"
-    frameborder="0"
-    width="800"
-    height="600"
-    allowtransparency>
-</iframe>
+<div></div>
 @endsection
